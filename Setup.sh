a918f0bf76d094a82ea28e8f7692e699ba37ecd3
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2023-11-20 16:42:42 +0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -12,11 +12,11 @@ sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
 # ./ngrok authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
 # ./ngrok tcp 22 --log ".ngrok.log" &
 # sleep 10
-# echo "Ngrok Setup Completeed"
+# 
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
   sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
   echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
   sudo tee /etc/apt/sources.list.d/ngrok.list && \
   sudo apt update && sudo apt install ngrok
 ngrok config add-authtoken 1n391cGDskZaeIjGacWKh0NS93J_83n7ttgsiQJzVtV2R5nn1
-ngrok http http://localhost:8080
+echo "Ngrok Setup Completeed"
