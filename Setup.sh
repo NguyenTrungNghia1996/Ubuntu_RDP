29dc14c56d1f9e3c144fa91e0434c916a3536ad4
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2023-11-20 16:34:43 +0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,15 +1,22 @@
-# Add user & setup ngrok
+# # Add user & setup ngrok
 sudo useradd -m $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)
 sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) sudo
 echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $GITHUB_EVENT_PATH) | sudo chpasswd
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-chmod +x ./ngrok
-echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
-rm -f .ngrok.log
-./ngrok authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
-./ngrok tcp 22 --log ".ngrok.log" &
-sleep 10
-echo "Ngrok Setup Completeed"
+# wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+# unzip ngrok-stable-linux-386.zip
+# chmod +x ./ngrok
+# echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)\n$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | sudo passwd "$USER"
+# rm -f .ngrok.log
+# ./ngrok authtoken $(jq -r '.inputs.authtoken' $GITHUB_EVENT_PATH)
+# ./ngrok tcp 22 --log ".ngrok.log" &
+# sleep 10
+# echo "Ngrok Setup Completeed"
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | \
+  sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && \
+  echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | \
+  sudo tee /etc/apt/sources.list.d/ngrok.list && \
+  sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 1n391cGDskZaeIjGacWKh0NS93J_83n7ttgsiQJzVtV2R5nn1
+ngrok http http://localhost:8080
